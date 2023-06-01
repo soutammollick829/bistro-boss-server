fa6173a30228a460c19400ae7ec045d0cdc1a351
soutammolick829 <soutammollick829@gmail.com>
2023-06-01 06:07:16 +0600

    add to cart transtack query

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const cors = require('cors');
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 //middlewere
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req,res)=>{
-    res.send('boss is sitting');
-})
-
-
+app.get("/", (req, res) => {
+  res.send("boss is sitting");
+});
 
 const uri = `mongodb+srv://${process.env.BOSS_USER}:${process.env.BOSS_PASS}@cluster10.dn0f8be.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -23,7 +21,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -31,15 +29,35 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const menuCollation = client.db('bossDB').collection('menu');
+    const menuCollation = client.db("bossDB").collection("menu");
+    const cartsCollation = client.db("bossDB").collection("carts");
 
-    app.get('/menu', async(req,res)=>{
-        const result = await menuCollation.find().toArray();
-        res.send(result);
-    })
+    app.get("/menu", async (req, res) => {
+      const result = await menuCollation.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/carts", async(req, res) => {
+      const email = req.query.email;
+      if(!email){
+        res.send([]);
+      }
+      const query = {email: email};
+      const result = await cartsCollation.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/carts", async (req, res) => {
+      const item = req.body;
+      console.log(item);
+      const result = await cartsCollation.insertOne(item);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -47,7 +65,6 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.listen(port , ()=>{
-    console.log(`boss is sitting on port:${port}`)
-})
+app.listen(port, () => {
+  console.log(`boss is sitting on port:${port}`);
+});
